5bb6c2a32c0d7450900a0239b86f221bd24cca02
Yevheniia Shabalova <evgenia.gq@gmail.com>
2019-05-21 16:12:05 +0300

    added to-dos

---
+++ src/app/toDo/to-do-list/to-do-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+// import { ToDoItem } from '../../to-do-item.model';
+
+@Component({
+  selector: 'app-to-do-list',
+  templateUrl: './to-do-list.component.html',
+  styleUrls: ['./to-do-list.component.scss']
+})
+export class ToDoListComponent implements OnInit {
+
+  // items: ToDoItem[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
